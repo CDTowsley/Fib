506472738647556bac59037b068b872fbd2e1858
Chris T <Chris T>
2017-08-08 19:33:55 -0700

    Add project files.

---
+++ Fib/CalcFib.cs
@@ -0,0 +1,22 @@
+﻿namespace Fib
+{
+    class CalcFib
+    {
+        public int CalculateFibonacci(int n)
+        {
+            // Set initial values
+            int returnValue = 0;
+            int nextFib = 1;
+
+            // loop through each step to calculate increase
+            for (int i = 0; i < n; i++)
+            {
+                int prevTotal = returnValue; // Store the previous steps total for next itteration 
+                returnValue = nextFib; // Set return value for this itteration
+                nextFib = prevTotal + nextFib; // Calculate the next step
+            }
+            return returnValue;
+        }
+
+    }
+}
---
+++ Fib/Fib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fib
+{
+    class Fib
+    {
+        static void Main()
+        {
+            CalcFib fib = new CalcFib();
+            Console.WriteLine("Fibonacci at 10: " + fib.CalculateFibonacci(10));
+            Console.WriteLine("Fibonacci at 40: " + fib.CalculateFibonacci(40));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
